85cee6118ebdf80d33f2805e730dd574a939fd43
Mwia <iansnr10@gmail.com>
2026-01-15 16:40:38 +0300

    add food method view and html form to check if adds to db

--- calorieapp/views.py
+++ calorieapp/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Food
 
 # Create your views here.
 def index(request):
     return render(request ,'index.html' )
+
+#add new food item with calorie count
+def add_food(request):
+    if request.method == 'POST':
+        food_name = request.POST.get('name')
+        calories = request.POST.get('calories')
+        Food.objects.create(name = food_name, calories = calories)
+    return redirect('index')
