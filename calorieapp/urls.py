be82c46d72a3fd184ac6dd2fad8ed4b1d423cac6
Mwia <iansnr10@gmail.com>
2026-01-15 18:51:47 +0300

    add remove function , delete button in html and url path

--- calorieapp/urls.py
+++ calorieapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("index", views.index, name='index'),
     path("add_food/", views.add_food, name='add_food'),
-    path("list_food", views.list_all_food_items, name='list_food'),
+    path("list_food/", views.list_all_food_items, name='list_food'),
+    path("remove_food/", views.remove_food_item, name='remove_food'),
 ]
 
--- calorieapp/views.py
+++ calorieapp/views.py
@@ -11,7 +11,7 @@ def add_food(request):
         name = request.POST.get('name')
         calories = request.POST.get('calories')
         Food.objects.create(name = name, calories = calories)
-    return redirect('index')
+    return redirect('list_food')
 
 #view a list of all food items added
 def list_all_food_items(request):
@@ -19,3 +19,10 @@ def list_all_food_items(request):
     return render(request, 'index.html', {'food_items': food_items})
 
 
+#remove food items, preferably on at a time
+def remove_food_item(request):
+    if request.method == 'POST':
+        food_id = request.POST.get('food_id')
+        food = Food.objects.get(id=food_id)
+        food.delete()
+    return redirect('list_food')
