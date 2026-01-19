c877cd9ac8ba3e9dd1b790f2a3a7930a477b6f68
Mwia <iansnr10@gmail.com>
2026-01-19 14:07:26 +0300

    changing url paths and adding new ones

--- calorieapp/urls.py
+++ calorieapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index", views.index, name='index'),
+    path("register/", views.register, name='register'),
+    path("index/", views.index, name='index'),
     path("add_food/", views.add_food, name='add_food'),
     path("list_food/", views.list_all_food_items, name='list_food'),
     path("remove_food/", views.remove_food_item, name='remove_food'),
